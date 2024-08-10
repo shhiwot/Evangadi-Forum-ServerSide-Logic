b6d111350875450f5dc2cd45d74b19e1441a4aad
shhiwot <shhiwot14@gmail.com>
2024-08-10 15:38:10 -0500

    update port number

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors =require("cors")
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const authMiddleware = require("./middleware/authMiddleware");
 
 
